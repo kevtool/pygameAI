16ca2247a40597c6abab976e23c071fc13049882
Kevin Lim <kevinlim9171@gmail.com>
2025-01-14 23:16:13 -0800

    reworking step, etc

--- game.py
+++ game.py
@@ -1,9 +1,10 @@
 import pygame
 import pygame.freetype
+from pygameai import pygameAI
 from objects import Ship, Pipe
 from algorithm import Algorithm
 
-class Game():
+class Game(pygameAI):
     def __init__(self):
         self.window_width = 1280
         self.window_height = 720
@@ -23,6 +24,10 @@ class Game():
         # tracking direction change
         self.prev_action = None
         self.changes = 0
+
+        # obs / action space
+        self.set_obs_space('discrete', 4)
+        self.set_action_space('discrete', 2)
 
     def initiate_pygame(self, render=True):
         pygame.init()
@@ -106,20 +111,22 @@ class Game():
 
     # the step function of the game needs to take in action as an argument
     # if the mode is human, we can then get action from pygame keys and ignore the argument
-    def step(self, mode='human', brain=None, render=True):
+    def step(self, action, mode='human', brain=None, render=True):
+        assert mode in ['human', 'ai']
+
         keys = pygame.key.get_pressed()
 
         nearest_pipe_topend, nearest_pipe_bottomend, nearest_pipe_x = self.get_nearest_pipe_info()
 
         if mode == 'human':
-            action = keys[pygame.K_SPACE]
+            action_ = 1 if keys[pygame.K_SPACE] else 0
         elif mode == 'ai':
-            action = (brain.forward(self.normalize_values(self.player_pos.y, nearest_pipe_topend, nearest_pipe_bottomend, nearest_pipe_x)) > 0)
-        else:
-            raise Exception("Error: Invalid mode")
+            # action = (brain.forward(self.normalize_values(self.player_pos.y, nearest_pipe_topend, nearest_pipe_bottomend, nearest_pipe_x)) > 0)
+            action_ = action
+
+        assert action_ in self.action_space
         
-        
-        if action:
+        if action_:
             self.player.update_pos('up')
         else:
             self.player.update_pos('down')
@@ -127,11 +134,11 @@ class Game():
         change = 0
 
         if self.prev_action != None:
-            if self.prev_action != action:
+            if self.prev_action != action_:
                 change = 1
                 self.changes += 1
             
-        self.prev_action = action
+        self.prev_action = action_
         
         self.player_pos.y = self.player.pos
         
@@ -173,6 +180,8 @@ class Game():
         self.clock.tick(self.game_speed)
     
     def run(self, iters, mode='human', brain=None, game_speed=60, render=True):
+        assert mode in ['human', 'ai']
+        
         scores = []
         dir_changes = []
 
--- pygameai.py
+++ pygameai.py
@@ -2,8 +2,21 @@ class pygameAI():
     def __init__(self):
         pass
 
-    def get_action(self, observation):
-        return self.model.get_action(observation)
+    def set_action_space(self, type, num):
+        assert type in ['discrete', 'continuous']
+        self.action_type = type
+        if type == 'discrete':
+            self.action_space = [i for i in range(num)]
+        elif type == 'continuous':
+            pass
+
+    def set_obs_space(self, type, num):
+        assert type in ['discrete', 'continuous']
+        self.obs_type = type
+        if type == 'discrete':
+            self.obs_space = [i for i in range(num)]
+        elif type == 'continuous':
+            pass
 
     def reset(self):
         pass
